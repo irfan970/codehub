9c00e8bde1352a229b41d0ba6a14310a8a5936d4
Irfan970 <irfanali970@yahoo.com>
2020-10-27 15:55:08 +0500

    practic file commited

---
+++ sass-practice-file-1/js/main.js
@@ -0,0 +1,28 @@
+   // <!--intialize anuimation on scroll -->
+      
+   AOS.init(
+    {
+      duration:600,
+      
+
+    }
+  );
+
+// <!--owl crousal j-Query code -->
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+  items:3,
+  loop:true,
+  margin:10,
+  autoplay:true,
+  autoplayTimeout:3000,
+  autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+  owl.trigger('play.owl.autoplay',[3000])
+})
+$('.stop').on('click',function(){
+  owl.trigger('stop.owl.autoplay')
+})
+
+
